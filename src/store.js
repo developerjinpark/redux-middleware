b28f414c491a41d16f9d90a358fdbce398b42c4d
Jin <jinpark1504@gmail.com>
2020-01-02 19:40:19 -0500

    use redux-logger library

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
 import modules from './modules';
-import loggerMiddleware from './lib/loggerMiddleware';
+import { createLogger } from 'redux-logger';
 
-const store = createStore(modules, applyMiddleware(loggerMiddleware));
+const logger = createLogger();
+
+const store = createStore(modules, applyMiddleware(logger));
 
 export default store;
