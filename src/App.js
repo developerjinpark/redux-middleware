133dd6433de7fc3834f511dfc01e8c8d37e98416
Jin <jinpark1504@gmail.com>
2020-01-03 20:04:12 -0500

    web request by using redux- axios and thunk

--- src/App.js
+++ src/App.js
@@ -2,18 +2,68 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+// import axios from 'axios';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
+    // loadData = () => {
+    //     const { PostActions, number } = this.props;
+    //     PostActions.getPost(number).then(
+    //         (response) => {
+    //             console.log(response);
+    //         }
+    //     ).catch(
+    //         (error) => {
+    //             console.log(error);
+    //         }
+    //     )
+    // }
+
+    // using ES7
+    loadData = async () => {
+        const { PostActions, number } = this.props;
+        try {
+            const response = await PostActions.getPost(number);
+            console.log(response);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+    
+    componentDidMount() {
+        // axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        //     .then(response => console.log(response));
+        this.loadData();
+    }
+    componentDidUpdate(prevProps, prevState) {
+        if(this.props.number !== prevProps.number) {
+            this.loadData();
+        }
+    }
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, loading, error } = this.props;
 
         
         return (
             <div>
                 <h1>{number}</h1>
-                <button onClick={CounterActions.incrementAsync}>+</button>
-                <button onClick={CounterActions.decrementAsync}>-</button>
+                {
+                    loading 
+                        ? (<h2>loading...</h2>) 
+                        : (
+                            error 
+                                ? (<h2>error!</h2>) 
+                                : (
+                                    <div>
+                                        <h2>{post.title}</h2>
+                                        <p>{post.body}</p>
+                                    </div>
+                                )
+                        )
+                }
+                <button onClick={CounterActions.increment}>+</button>
+                <button onClick={CounterActions.decrement}>-</button>
             </div>
         );
     }
@@ -21,9 +71,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions: bindActionCreators(postActions, dispatch)
     })
 )(App);
---
+++ src/modules/post.js
@@ -0,0 +1,65 @@
+import { handleActions, createAction } from 'redux-actions';
+// using axios
+import axios from 'axios';
+
+function getPostAPI(postId) {
+    return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
+}
+
+const GET_POST_PENDING = 'GET_POST_PENDING';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'GET_POST_FAILURE';
+
+const getPostPending = createAction(GET_POST_PENDING);
+const getPostSuccess = createAction(GET_POST_SUCCESS);
+const getPostFailure = createAction(GET_POST_FAILURE);
+// using thunk
+export const getPost = (postId) => dispatch => {
+    dispatch(getPostPending());
+
+    return getPostAPI(postId).then((response) => {
+        dispatch(getPostSuccess(response));
+
+        return response;
+    }).catch(error => {
+        dispatch(getPostFailure(error));
+
+        throw(error);
+    });
+}
+// initial reducer
+const initialState = {
+    pending: false,
+    error: false,
+    data: {
+        title: '',
+        body: ''
+    }
+}
+
+export default handleActions({
+    [GET_POST_PENDING]: (state, actions) => {
+        return {
+            ...state,
+            pending: true,
+            error: false
+        };
+    },
+    [GET_POST_SUCCESS]: (state, actions) => {
+        const { title, body } = actions.payload.data;
+        return {
+            ...state,
+            pending:false,
+            data: {
+                title, body
+            }
+        };
+    },
+    [GET_POST_FAILURE]: (state, action) => {
+        return {
+            ...state,
+            pending: false,
+            error: true
+        }
+    }
+}, initialState);
